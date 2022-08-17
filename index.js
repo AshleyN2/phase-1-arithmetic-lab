f376ae864b1a699b3d15cba79713ccf98ca0e60f
Ashley <ashleynjeri7@gmail.com>
2022-08-18 02:31:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply= num1 * num2;;
+
+// Random integer > 0
+const random = Math.floor(Math.random()*10);
+
+// Mod
+const num3 = 100;
+const num4 = 96;
+const mod = num3 % num4;;
+
+// Max
+const max = Math.max(14,3,8,20,11);
